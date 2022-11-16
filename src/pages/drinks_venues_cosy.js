b71225118f97ac3ab97edd780d16fbc334de8860
Boerie202 <matthew.hanbury@outlook.com>
2022-11-16 16:14:43 +0000

    Added json data to drinks_venues_cosy.js
    Everything is pulling through, just need to figure out how to integrate it with the css I need

--- src/pages/drinks_venues_cosy.js
+++ src/pages/drinks_venues_cosy.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Container from 'react-bootstrap/Container';
+// import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import 'bootstrap/dist/css/bootstrap.css';
@@ -7,33 +7,41 @@ import 'bootstrap/dist/css/bootstrap.css';
 import './AllPages.css'
 
 //Data Imports
-import { CosyDrinksData } from './data.js';
+// import { CosyDrinksData } from './data.js';
 
 //JSON import
 import Venues from './venue_data';
 
 
-
-function DrinksCosy() {
+function DrinksCosy () {
     return (
-        <div className='page-container'>
+        <div className='page-container fluid'>
             <h2 className='page-title'>
                 Cosy Vibes
             </h2>
 
             <Row className='d-flex align-items-center justify-content-center venue-page'>
-                {Venues.map((data, key) => {
+
+                {Venues && Venues.map((venue) => {
                     return (
-                        <h2 key={key}> {data.name} </h2>,
-                        <h6 key={key}> {data.address} </h6>
+                        <div key = {venue.id}>
+                    
+                                <h2> {venue.name} </h2>
+                                <h6> {venue.address} </h6>
+                                
+                                 <h3>Perks</h3>
+                                    <p className='p1'> {venue.perks[0]} </p> <br />
+                                    <p className='p2'> {venue.perks[1]} </p> <br />
+                                    <p className='p3'> {venue.perks[2]} </p>
+                            
+                            <Col className='additional-info-list'><li>Gallery</li><li className='coming-soon'>Coming Soon</li><li>Menu</li><li className='coming-soon'>Coming Soon</li></Col>
+                        
+                        </div>
                     );
                 })}
             </Row>
-            {/* <CosyDrinksData /> */}
-
-
         </div>
-
+        
     );
 }
 
